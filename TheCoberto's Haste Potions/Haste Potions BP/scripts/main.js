b868693878bb5f6e151509b6dc4274574d2e6372
wkelly <coby.kelly.ck@gmail.com>
2024-10-07 08:37:56 -0400

    revert to thecoberto:haste_potion

--- TheCoberto's Haste Potions/Haste Potions BP/scripts/main.js
+++ TheCoberto's Haste Potions/Haste Potions BP/scripts/main.js
@@ -5,7 +5,7 @@ world.afterEvents.itemCompleteUse.subscribe(use => {
         let p = use.source;
         let item = use.itemStack;
 
-        if (item.typeId == "new:haste_potion") {
+        if (item.typeId == "thecoberto:haste_potion") {
             p.runCommandAsync("effect @s haste 1800 1 false")
         }
 
