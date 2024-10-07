01e7975df0b1c89278df11a25a4744fba6e14b1c
wkelly <coby.kelly.ck@gmail.com>
2024-10-06 23:36:42 -0400

    update format version of item

---
+++ TheCoberto's Haste Potions/Haste Potions BP/scripts/main.js
@@ -0,0 +1,15 @@
+import { world } from "@minecraft/server";
+
+world.afterEvents.itemCompleteUse.subscribe(use => {
+    try {
+        let p = use.source;
+        let item = use.itemStack;
+
+        if (item.typeId == "new:haste_potion") {
+            p.runCommandAsync("effect @s haste 1800 1 false")
+        }
+
+    } catch (e) {
+
+    }
+})
